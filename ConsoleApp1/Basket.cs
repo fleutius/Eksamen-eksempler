45b8f89e3e8c9db72af740f376e0c8f0ba5e8543
fleutius <flemming88@gmail.com>
2022-06-04 22:25:04 +0200

    Add project files.

---
+++ ConsoleApp1/Basket.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //Base Basket class
+    class Basket
+    {
+
+        protected int _baskettotal;
+        
+        //Getter for _baskettotal. the field has no public setter here, since this is done later 
+        public int BasketTotal {
+            get
+            {
+                return this._baskettotal;
+            }
+        }
+
+        public int? CustId { get; set; }
+
+        public List<string> ItemList = new List<string>();
+
+        public int total = 0;
+
+        //Adds selected items to ItemList and functions as Setter for _baskettotal
+        public virtual void AddItem(string ProductName, int ProductId, int ProductPrice, int Quantity)
+        {
+            ItemList.Add(ProductName);
+            _baskettotal += (ProductPrice * Quantity);
+        }
+
+        // Loops over items in ItemList and prints them to screen
+        public void GetItems()
+        {
+            foreach (var i in ItemList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public virtual void AmountDue()
+        {
+            GetItems();
+            Console.WriteLine("Total amount for Order: " + BasketTotal + " Inc VAT");
+        }
+
+
+
+    }
+    // BusinessBasket overrides AmountDue in order to show the user that the total is excluding VAT
+    class BusinessBasket:Basket
+    {
+        public override void AmountDue()
+        {
+            GetItems();
+            Console.WriteLine("Total amount for Order: " + BasketTotal + " Excl. VAT");
+        }
+    }
+
+    //ConsumerBasket overrides AddItem in order to add VAT to the total shown in Main
+    class ConsumerBasket : Basket
+    {
+        public int Vat { get; set; } = 25;
+
+        static int VatPercentage(int Vat)
+        {
+            return (Vat / 100) + 1;
+        }
+        public override void AddItem(string ProductName, int ProductId, int ProductPrice, int Quantity)
+        {
+            ItemList.Add(ProductName);
+            _baskettotal += (ProductPrice * Quantity) + VatPercentage(Vat) ;
+        }
+    }
+
+}
---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Products used in code
+            Product Prod1 = new Product();
+            Prod1.ProductName = "Lim";
+            Prod1.ProductId = 1;
+            Prod1.ProductPrice = 10;
+            
+            Product Prod2 = new Product();
+            Prod2.ProductName = "Stolpe";
+            Prod2.ProductId = 2;
+            Prod2.ProductPrice = 20;
+
+            //Cust1 - show of Consumer Customer
+            Customer Cust1 = new Customer();
+            Cust1.Fname = "Jens";
+            Cust1.Email = "jens@jems.dk";
+            Cust1.CustID = 0001;
+
+            //Cust2 - Show of Business Customer
+            Customer Cust2 = new Customer();
+            Cust2.Fname = "Jysk";
+            Cust2.Email = "info@jysk.dk";
+            Cust2.CustID = 0002;
+
+            ConsumerBasket Cust1Basket = new ConsumerBasket();
+            Cust1Basket.CustId = Cust1.CustID;
+
+            Cust1Basket.AddItem(Prod1.ProductName, Prod1.ProductId, Prod1.ProductPrice, 2);
+
+            Cust1Basket.AddItem(Prod2.ProductName, Prod2.ProductId, Prod2.ProductPrice, 4);
+
+            Console.WriteLine("Showing Basket items and total for " + Cust1.Fname);
+            Cust1Basket.AmountDue();
+
+            Console.WriteLine("---------------------------------------------------");
+
+            BusinessBasket Cust2Basket = new BusinessBasket();
+            Cust2Basket.CustId = Cust2.CustID;
+            Cust2Basket.AddItem(Prod1.ProductName, Prod1.ProductId, Prod1.ProductPrice, 2);
+
+            Cust2Basket.AddItem(Prod2.ProductName, Prod2.ProductId, Prod2.ProductPrice, 4);
+            Console.WriteLine("Showing Basket items and total for " + Cust2.Fname);
+            Cust2Basket.AmountDue();
+            Console.WriteLine("---------------------------------------------------");
+
+        }
+    }
+}
